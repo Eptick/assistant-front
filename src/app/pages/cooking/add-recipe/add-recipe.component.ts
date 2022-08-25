df602e2a3bd382260c48a0056808f406c17717d0
Eptick <redzicleon@gmail.com>
2022-08-25 22:24:37 +0200

    Add recipe

--- src/app/pages/cooking/add-recipe/add-recipe.component.ts
+++ src/app/pages/cooking/add-recipe/add-recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { catchError, EMPTY } from 'rxjs';
 import { RecipesService } from 'src/app/api/cooking/recipes.service';
 import { Ingredient } from 'src/app/types/shared/ingredient.types';
@@ -10,16 +11,52 @@ import { Ingredient } from 'src/app/types/shared/ingredient.types';
   styleUrls: ['./add-recipe.component.scss']
 })
 export class AddRecipeComponent implements OnInit {
-  selectedIngredients: Ingredient[] = [];
+  _selectedIngredients: Ingredient[] = [];
 
   public form = new FormGroup({
     name: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
+    ingredients: new FormArray([]),
   });
 
-  constructor(private recipesService: RecipesService) { }
+  constructor(
+    private router: Router,
+    private recipesService: RecipesService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  get selectedIngredients(): Ingredient[] {
+    return this._selectedIngredients;
+  }
+
+  set selectedIngredients(ingredients: Ingredient[]) {
+    let quantityMap = this.ingredients.value.reduce((a: {[key: string]: string},b: Ingredient) => {
+      a[b.uuid] = b.quantity
+      return a;
+    }, {})
+    this.ingredients.clear();
+    this._selectedIngredients = ingredients;
+    ingredients = ingredients.map(elem => ({...elem, quantity: quantityMap[elem.uuid] ?? elem.quantity ?? ''}))
+    ingredients.forEach((i) => this.addIngredientToForm(i))
+  }
+
+  get ingredients() {
+    return this.form.controls["ingredients"] as FormArray;
+  }
+  
+  private addIngredientToForm(ingredient: Ingredient) {
+    let arr = this.form.controls['ingredients'] as FormArray
+    arr.push(this.getIngredientGroup(ingredient))
+  }
+
+  private getIngredientGroup(ingredient: Ingredient) {
+    return new FormGroup({
+      uuid: new FormControl(ingredient.uuid, [Validators.required]),
+      name: new FormControl(ingredient.name, [Validators.required]),
+      quantity: new FormControl(ingredient.quantity, [Validators.required])
+    })
   }
 
   onSubmit() {
@@ -28,7 +65,13 @@ export class AddRecipeComponent implements OnInit {
       console.table(this.form.value);
       this.recipesService.saveRecipe({
         ...this.form.value,
-        ingredients: this.selectedIngredients.map(e => ({ingredientUUID: e.uuid, quantity: 'dva'}))
+        ingredients: this.ingredients.controls.map((e) => {
+          const g: FormGroup = e as FormGroup
+          const ingredientUUID = g.controls['uuid'].value
+          const quantity = g.controls['quantity'].value
+          debugger
+          return {ingredientUUID, quantity: quantity}
+        })
       })
       .pipe(
         catchError(err => {
@@ -37,7 +80,7 @@ export class AddRecipeComponent implements OnInit {
           return EMPTY;
         })
       ).subscribe(data => {
-        // this.router.navigate(['/onboarding/provider/step-two']);
+        this.router.navigate(['/cooking']);
       })
     }
   }
